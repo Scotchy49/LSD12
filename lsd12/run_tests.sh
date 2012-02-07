e5114ce276ec9145be4506041625a7d5f8a3eaf9
Serkan Eryilmaz <serkan@earlytracks.com>
2012-02-07 13:59:06 +0100

    improved tester to make it show the error on KO (if it was expected only). To double check the test passes because of the correct reason

--- lsd12/run_tests.sh
+++ lsd12/run_tests.sh
@@ -5,7 +5,12 @@ while IFS=' ' read -ra TOKENS; do
     result=`./lsd12 < ${TOKENS[1]} 2>&1 | head -1`
     if [ "$result" = "${TOKENS[0]}" ] 
     then
-        echo "${TOKENS[1]} passed"
+        if [ "$result" = "KO" ] 
+        then
+            echo "${TOKENS[1]} passed with KO `./lsd12 < ${TOKENS[1]} 2>&1 | tail -1` $error"
+        else
+            echo "${TOKENS[1]} passed with OK"
+        fi
     else
         echo "${TOKENS[1]} failed: Got $result, expected ${TOKENS[0]}."
     fi
